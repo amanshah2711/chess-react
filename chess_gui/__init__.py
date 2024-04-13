a9f7761a7a2724e6fc5ae9b877d186569a9a03b4
Aman <amanshah2711@berkeley.edu>
2024-04-12 22:44:34 -0400

    Setting environment variables:

--- chess_gui/__init__.py
+++ chess_gui/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_socketio import SocketIO, send, emit
 from chess_gui import assets
+import os
+
 app = Flask(__name__, static_url_path='/chess_gui/static')
-app.config['SECRET_KEY'] = 'secret!'
-app.config['DEBUG'] = True 
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "secret!")
+app.config['DEBUG'] = os.getenv("DEBUG", True)
 app.jinja_env.globals.update({
   'assets_env': assets.assets_env,
 })
